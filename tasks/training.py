312e2a7facffbbaf21db50211de3be8b239b2b6d
Kylian Schmidt <kylian.schmidt@kit.edu>
2025-06-26 16:13:42 +0200

    Move training task and add law file targets

---
+++ tasks/training.py
@@ -0,0 +1,36 @@
+import time
+import os
+import json
+
+import law
+
+from ml.config import MLConfig
+from preprocessor.ingestion.formatter import Ingestor
+from orchestrator.train import TrainingBase
+
+
+class TrainingBaseTask(law.Task):
+
+    config_yaml = law.Parameter(
+        description="Path to the YAML configuration file for the training.",
+        default="ml/config/config.yaml"
+    )
+
+    def output(self):
+        time_stamp = int(time.time())
+        base_dir = os.path.join("training_output", f"{time_stamp}")
+        return {
+            "logs": law.LocalDirectoryTarget(f"{base_dir}/tensorboard_logs"),
+            "outputs": law.LocalFileTarget(f"{base_dir}/outputs"),
+        }
+
+    def run(self):
+        self.config = MLConfig.from_yaml(self.config_yaml, strict=True)  # type: ignore
+        training_base = TrainingBase(
+            ingestor=Ingestor.from_parquet(self.config.files_to_load),
+            config=self.config
+        )
+        outputs = training_base.train()
+
+        with open(os.path.join(self.output()["outputs"], "training_output.json"), "w") as f:
+            json.dump(outputs, f, indent=4)
