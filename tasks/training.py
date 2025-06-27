a1ca9271a48affcdd461924050f7ac7f6c391ce8
Kylian Schmidt <kylian.schmidt@kit.edu>
2025-06-27 16:07:17 +0200

    Add logger. Add Tensorboard log dir as law.Parameter

--- tasks/training.py
+++ tasks/training.py
@@ -1,12 +1,14 @@
-import time
 import os
 import json
+import logging
 
 import law
 
 from ml.config import MLConfig
 from preprocessor.ingestion.formatter import Ingestor
 from orchestrator.train import TrainingBase
+
+logger = logging.getLogger(__name__)
 
 
 class TrainingBaseTask(law.Task):
@@ -15,31 +17,41 @@ class TrainingBaseTask(law.Task):
         description="Path to the YAML configuration file for the training.",
         default="ml/config/config.yaml"
     )
+    results_dir = law.Parameter(
+        description="Directory where the training results will be saved.",
+        default="runs",
+        significant=False,
+    )
 
     def output(self):
-        time_stamp = int(time.time())
-        base_dir = os.path.join("training_output", f"{time_stamp}")
         return {
-            "logs": law.LocalDirectoryTarget(f"{base_dir}/tensorboard_logs"),
-            "outputs": law.LocalFileTarget(f"{base_dir}/outputs"),
+            "logs": law.LocalDirectoryTarget(f"{self.results_dir}/tensorboard_logs"),
+            "outputs": law.LocalFileTarget(f"{self.results_dir}/outputs"),
         }
 
     def run(self):
         self.config = MLConfig.from_yaml(self.config_yaml, strict=True)  # type: ignore
+
+        if self.config.device == "cpu":
+            logger.warning("Running on CPU. This may be slow for large datasets.")
+
         training_base = TrainingBase(
             features_ingestor=Ingestor.read(
                 self.config.files_to_load,
                 format=self.config.filetype,
                 columns=self.config.features_columns,
                 reader_kwargs=self.config.dak_reader_kwargs,
+                max_number_events=self.config.max_number_events,
             ),
             labels_ingestor=Ingestor.read(
                 self.config.files_to_load,
                 format=self.config.filetype,
                 columns=self.config.labels_columns,
                 reader_kwargs=self.config.dak_reader_kwargs,
+                max_number_events=self.config.max_number_events,
             ),
             config=self.config,
+            tensor_board_log_dir=self.output()["logs"].path,
         )
         outputs = training_base.train()
 
