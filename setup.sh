e256c61c095ae600eb09e769bd2c286ded5b6638
Kylian Schmidt <kylian.schmidt@kit.edu>
2025-07-14 12:47:38 +0200

    Add singularity .def files for environment

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
 export LAW_HOME=$(pwd)
 export LAW_CONFIG_FILE="$LAW_HOME/law.cfg"
 export PYTHONPATH="./preprocessor/:ml:.:$PYTHONPATH"  # blame it on LAW that we have to overwrite PYTHONPATH
-source "$(law completion)"
+# Only source completion if we're in an interactive bash shell
+if [[ $- == *i* ]] && [[ -n "$BASH_VERSION" ]]; then
+    . "$(law completion)" 2>/dev/null || true
+fi
 echo -e "\e[32mNEEDLE Setup successful!\e[0m"
