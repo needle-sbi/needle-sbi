50c1e72965cedf33aca9e3122acf333d73f794c3
Kylian Schmidt <kylian.schmidt@kit.edu>
2025-10-23 08:40:30 +0200

    Add tests for running law tasks, e.g. EnsembleTask

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
-import pytest
-from pathlib import Path
 import os
+from pathlib import Path
+
+import pytest
+
+from ml.utils.config import MLConfig
 
 
 @pytest.fixture(scope="session")
@@ -12,8 +15,40 @@ def fair_universe_sample() -> str:
             "Environment variable 'FAIR_UNIVERSE_DATA' not set. Should point to a parquet file: "
             "'export FAIR_UNIVERSE_DATA=../fair_universe/input_data/train/data/data.parquet'."
         )
-    
+
     if not Path(path).exists():
         pytest.skip(f"Path {path} does not exist")
-    
+
     return path
+
+
+@pytest.fixture
+def config_yaml(
+    tmp_path: Path,
+    request: pytest.FixtureRequest,
+) -> str:
+    pytest.importorskip("preprocessor", reason="Could not import 'preprocessor'")
+    from preprocessor.tests.conftest import ArrayField
+
+    try:
+        make_parquet_file = request.getfixturevalue("make_parquet_file")
+    except pytest.FixtureLookupError:
+        pytest.skip("Fixture 'make_parquet_file' from preprocessor could not be found")
+
+    template = ArrayField(dtype=float, shape=(10000, 1, 1))
+    file = make_parquet_file(
+        columns={
+            "Lepton": {"pt": template},
+            "Jet": {"eta": template},
+        },
+        file_name="simple",
+    )
+
+    config = MLConfig()
+    config.files_to_load = [file]
+    config.total_epoch = 1
+    config.features_columns = ["Lepton.pt"]
+    config.labels_columns = ["Jet.eta"]
+    config_path = os.path.abspath(tmp_path / "config.yaml")
+    config.to_yaml(config_path)
+    return config_path
---
+++ tests/trainings/test_ensemble_task.py
@@ -0,0 +1,24 @@
+"""
+Test the execution of the k-fold training Tasks
+
+NOTE This Task is called EnsembleTask but it wraps the k-fold Training Tasks, which is why we
+use if in this test. Once the EnsembleTask has more functionality than just calling several
+FoldTasks, we can refactor this test.
+"""
+import pytest
+
+from law_tasks.ensemble import EnsembleTask
+from ml.utils.config import MLConfig
+
+
+@pytest.mark.law
+def test_kfold_training(
+    config_yaml: str,
+):
+    config = MLConfig.from_yaml(config_yaml)
+    config.n_folds = 2
+    config.to_yaml(config_yaml)
+
+    ensemble = EnsembleTask()
+    ensemble.config_yaml = config_yaml  # type: ignore
+    assert ensemble.law_run(remove_output=[2, "a", "y"])
