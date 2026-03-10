9931edc86786a50c25946a41d2dd8ccf72ef8a05
Kylian Schmidt <kylian.schmidt@kit.edu>
2026-03-10 13:47:32 +0100

    Add law tasks to __init__ for easier lookup

--- law_tasks/__init__.py
+++ law_tasks/__init__.py
@@ -1,6 +1,16 @@
 from .training_base import TrainingBaseTask
+from .main import MainTask
+from .estimator import EstimatorTask
+from .ensemble import EnsembleTask
+from .systematic import SystematicTask
+from .fold import FoldTask
 
 
 __all__ = [
     "TrainingBaseTask",
+    "MainTask",
+    "EstimatorTask",
+    "EnsembleTask",
+    "SystematicTask",
+    "FoldTask",
 ]
