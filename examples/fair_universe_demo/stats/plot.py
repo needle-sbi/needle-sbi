aa0dcd4b1903810768ac3960c6d5d7ed3f5acd49
Kylian Schmidt <kylian.schmidt@kit.edu>
2026-04-16 09:06:40 +0200

    Prettify plots

--- examples/fair_universe_demo/stats/plot.py
+++ examples/fair_universe_demo/stats/plot.py
@@ -14,6 +14,7 @@ from typing import Any, Callable, Dict, List
 
 import luigi
 import matplotlib.pyplot as plt
+import mplhep
 import numpy as np
 from matplotlib.figure import Figure
 from sklearn.metrics import roc_auc_score, roc_curve
@@ -69,6 +70,17 @@ class PlottingTask(luigi.Task):
             for plot_name in self.registered_plots.keys()
         }
 
+    def set_needle_plot_style(self, fig: Figure) -> Figure:
+        ax = fig.axes[0]
+        mplhep.label.exp_label(
+            loc=0,
+            exp="NEEDLE",
+            ax=ax,
+            rlabel="FAIR Universe HiggsML",
+        )
+        plt.tight_layout()
+        return fig
+
     @staticmethod
     def plot(
         *,
@@ -96,7 +108,7 @@ class PlottingTask(luigi.Task):
             setattr(func, "_plot_name", name or func.__name__)
 
             @wraps(func)
-            def wrapper(self: luigi.Task, *args, **kwargs) -> Figure:
+            def wrapper(self: "PlottingTask", *args, **kwargs) -> Figure:
                 """Wraps the call signature of the function so that the plot is automatically saved.
 
                 Raises:
@@ -110,6 +122,7 @@ class PlottingTask(luigi.Task):
                 if not isinstance(fig, Figure):
                     raise TypeError(f"Function {func.__name__} must return matplotlib.figure.Figure")
 
+                fig = self.set_needle_plot_style(fig)
                 save_path = self.output()[getattr(func, "_plot_name")].path
                 fig.savefig(save_path)
                 plt.close(fig)
@@ -149,19 +162,26 @@ class PlottingTask(luigi.Task):
             ground_truth_mu (dict): A dictionary of ground truth mu values.
             savepath (str): Where to save the resulting plot. If None (default), show the plot instead.
         """
-        fig = plt.figure(figsize=(6, 4))
+        fig, ax = plt.subplots(figsize=(5, 4), dpi=600)
+        xlims = (0, 3)
 
         for test_set_index, ingestion_result in ingestion_result_dict.items():
             mu_hat = np.mean(ingestion_result["mu_hat"])
             mu = ground_truth_mu[int(test_set_index)]
-            plt.scatter(mu, mu_hat, c="b", marker="o")
+            plt.scatter(mu, mu_hat, c="k", marker="+")
 
-        plt.xlabel("Ground Truth $\\mu$")
-        plt.ylabel("Predicted $\\mu$ (averaged over test sets)")
-        plt.title("Mu Prediction")
+        x = np.linspace(*xlims)
+        ax.plot(x, x, linestyle="--", label=r"$y=x$")
+
+        ax.legend(loc="upper left", title=r"$H \to \tau \tau$ (MC) averaged")
+
+        ax.set_xlim(*xlims)
+        ax.set_ylim(-4, 4)
+
+        ax.set_xlabel(r"$\mu_{\text{true}}$", loc="right")
+        ax.set_ylabel(r"$\mu_{\text{predicted}}$)", loc="top")
         return fig
 
-    @plot(name="roc_curve")
     def roc_curve_wrapper(
         self,
         score: np.ndarray,
@@ -183,7 +203,7 @@ class PlottingTask(luigi.Task):
             * color (str, optional): The color. Defaults to "b".
             * lw (int, optional): The line width. Defaults to 2.
         """
-        fig = plt.figure(figsize=(8, 7))
+        fig, ax = plt.subplots(figsize=(5, 4), dpi=600)
 
         auc = roc_auc_score(y_true=labels, y_score=score, sample_weight=weights)
         fpr, tpr, _ = roc_curve(y_true=labels, y_score=score, sample_weight=weights)
