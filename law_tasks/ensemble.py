c2ccac945181ee98168ecaa39efb411341b6894e
Kylian Schmidt <kylian.schmidt@kit.edu>
2026-04-17 16:24:12 +0200

    Add case distinction for remote inputs

--- law_tasks/ensemble.py
+++ law_tasks/ensemble.py
@@ -4,7 +4,7 @@ Task for a single ensemble training, includes multiple folds.
 
 import os
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 import law
 import luigi
@@ -16,6 +16,8 @@ from orchestrator.results import EnsembleResults, FoldResults
 from preprocessor.utils import ColorFormatter
 
 logger = ColorFormatter.get_logger("ensemble")
+
+FoldTaskOutput = List[Dict[str, law.LocalFileTarget] | Dict[str, law.TargetCollection]]
 
 
 class EnsembleTask(HydraMixin, law.Task):
@@ -72,7 +74,47 @@ class EnsembleTask(HydraMixin, law.Task):
             "outputs": base.child("ensemble_results.json", type="f"),
         }
 
+    def input(self) -> List[Dict[str, law.LocalTarget]]:
+        """Unpack local and remote inputs
+
+         1. Local is simply the Dict defined in the output method of the FoldTask
+         2. Remote is instead a DotDict with 'collection' and 'jobs' fields.
+
+        Example:
+            print(super().input())
+            [
+                DotDict(
+                    {
+                        "jobs": law.LocalFileTarget(),
+                        "collection": law.TargetCollection(len=1)
+                    }
+                )
+            ]
+
+        Returns:
+            Dict[str, str]: Properly formatted output Dict with key:Target pairs
+        """
+        _flattened_fold_outputs = []
+        fold_inputs: FoldTaskOutput = super().input()
+
+        for fold_output in fold_inputs:
+            remote_collection: law.TargetCollection | None = fold_output.get("collection")  # type: ignore
+
+            if remote_collection:
+                if len(remote_collection) != 1:
+                    raise NotImplementedError(
+                        "Currently the usage of branches in FoldTask is not supported. Instead, folds "
+                        "have to be their own Task instance required by EnsembleTask."
+                    )
+                _flattened_fold_outputs.append(remote_collection[0])
+            else:
+                _flattened_fold_outputs.append(fold_output)
+
+        return _flattened_fold_outputs
+
     def run(self) -> None:
         """Gather results from child FoldTask and merge them into own result container"""
-        fold_results = [FoldResults.from_json(fold_output["outputs"].path) for fold_output in self.input()]
+        fold_results = [
+            FoldResults.from_json(fold_output["outputs"].path) for fold_output in self.input()  # type: ignore
+        ]
         EnsembleResults(folds=fold_results).to_json(self.output()["outputs"].path)
