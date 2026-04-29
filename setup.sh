914831fb3846caac8ec50aae25b5b5a61313d146
Stephen Jiggins <stephen.jiggins@cern.ch>
2026-04-29 18:59:32 +0200

    <Version-00-00-45>::   Moved export to before check of active NEEDLE environment coming from submission environment.

--- setup.sh
+++ setup.sh
@@ -54,6 +54,8 @@ fi
 # Check if the script was already sourced
 if [[ -n "$NEEDLE_ENV_ACTIVE" ]]; then
     echo -e "${_NEEDLE_GREEN}$ENV_NAME environment is already active.${_NEEDLE_NC}"
+    # Still export FAIR_UNIVERSE_DATA for HTCondor jobs (where getenv=True copies the NEEDLE_ENV_ACTIVE flag)
+    export FAIR_UNIVERSE_DATA="/data/dust/group/atlas/needle/FAIRUnv/UncertaintyChallenge_2024/ProcessedData_v1_2025-10-03/CombData-part0.parquet"
     return 0
 fi
 
