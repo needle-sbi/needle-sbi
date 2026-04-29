75fa1a43e8c6493aebe9fe25a2a4a882cd543c8c
Stephen Jiggins <stephen.jiggins@cern.ch>
2026-04-29 23:03:22 +0200

    <Version-00-00-48>::   HTCondor jobs now only submitted via Ensemble task.

--- law_tasks/estimator.py
+++ law_tasks/estimator.py
@@ -17,11 +17,13 @@ from preprocessor.utils.logging import ColorFormatter
 logger = ColorFormatter.get_logger("estimator")
 
 
-class EstimatorTask(HydraMixin, law.htcondor.HTCondorWorkflow, law.LocalWorkflow):
+class EstimatorTask(HydraMixin, law.LocalWorkflow):
     """
-    Estimator task that runs SystematicTasks.    
-    By default runs as HTCondor workflow (submits branches to cluster).
-    Use --EstimatorTask-workflow local to run branches locally.    """
+    Estimator task that coordinates SystematicTasks.
+    
+    Runs locally to coordinate systematic branches. The actual training
+    happens in FoldTasks which are submitted to HTCondor by EnsembleTask.
+    """
     rel_results_path: str = law.Parameter(
         description="Directory where the estimator results will be saved.",
         default="runs/estimator",
@@ -75,44 +77,6 @@ class EstimatorTask(HydraMixin, law.htcondor.HTCondorWorkflow, law.LocalWorkflow
             )
         # Workflow container: no requirements
         return []
-
-    def htcondor_output_directory(self):
-        """Directory for HTCondor job logs"""
-        return law.LocalDirectoryTarget(
-            os.path.join(os.getcwd(), ".law", "htcondor", self.task_family, self.estimator)
-        )
-    
-    def htcondor_bootstrap_file(self):
-        """Bootstrap script for remote environment setup"""
-        # Get absolute path to setup.sh in workspace root
-        task_dir = os.path.dirname(os.path.abspath(__file__))
-        workspace_root = os.path.dirname(task_dir)
-        return os.path.join(workspace_root, "setup.sh")
-    
-    def htcondor_job_config(self, config, job_num, branches):
-        """Configure HTCondor resources"""
-        config.custom_content.append(("request_cpus", "2"))
-        config.custom_content.append(("request_memory", "16000"))
-        config.custom_content.append(("request_runtime", "7200"))
-        config.custom_content.append(("request_GPUs", "0"))
-        config.custom_content.append(("getenv", "True"))
-        config.custom_content.append(("universe", "vanilla"))
-        
-        # Note: FAIR_UNIVERSE_DATA is exported in setup.sh
-        # Note: Do NOT set transfer_input_files - it breaks LAW's internal file transfer
-        
-        config.custom_content.append(("should_transfer_files", "YES"))
-        config.custom_content.append(("when_to_transfer_output", "ON_EXIT"))
-        config.custom_content.append(("transfer_output_files", ""))
-        
-        # Set actual log files instead of /dev/null
-        log_dir = self.htcondor_output_directory().path
-        branch_name = "_".join(str(b) for b in branches)
-        config.log = os.path.join(log_dir, f"job_{job_num}_br{branch_name}.log")
-        config.stdout = os.path.join(log_dir, f"job_{job_num}_br{branch_name}.out")
-        config.stderr = os.path.join(log_dir, f"job_{job_num}_br{branch_name}.err")
-        
-        return config
     
     def output(self) -> Dict[str, Any]:
         if self.is_branch():
