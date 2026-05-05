fix: Standardize model and data precision to float32

examples/fair_universe_demo/fair_universe_demo/tasks/histogram.py
@@ -45,7 +45,7 @@ def _compute_histogram_entry(args):
     n_params = [1, 1, 1, j, i, 0]
 
     nf_ckpts, classifier_ckpt = HistogramTask.parse_snapshot(snapshot_path)
-    nf_models = ClassifierDatamodule.load_nf_models(nf_ckpts).to(device)
+    nf_models = ClassifierDatamodule.load_nf_models(nf_ckpts).to(device).eval().to(torch.float32)
     classifier = CombinedClassifier.load_from_checkpoint(classifier_ckpt["classifier"])
     classifier = classifier.to(device).eval().to(torch.float32)
 

examples/fair_universe_demo/fair_universe_demo/tasks/neyman.py
@@ -105,8 +105,13 @@ class NeymanTask(PlottingMixin):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
         nf_ckpts, classifier_ckpt = HistogramTask.parse_snapshot(self.snapshot_path)
-        self.nf_models = ClassifierDatamodule.load_nf_models(nf_ckpts)
-        self.classifier = CombinedClassifier.load_from_checkpoint(classifier_ckpt["classifier"])
+        self.nf_models = ClassifierDatamodule.load_nf_models(nf_ckpts).to(self.device).eval().to(torch.float32)
+        self.classifier = (
+            CombinedClassifier.load_from_checkpoint(classifier_ckpt["classifier"])
+            .to(self.device)
+            .eval()
+            .to(torch.float32)
+        )
 
         self.loaded_data = fetch_dataset(root_dir=self.root_dir)
 

examples/fair_universe_demo/fair_universe_demo/utils/eval.py
@@ -95,7 +95,7 @@ def predict(
     std_corrected_interp, a, b = load_bias_data(neyman_path)
 
     nf_ckpts, classifier_ckpt = HistogramTask.parse_snapshot(snapshot_path)
-    models = ClassifierDatamodule.load_nf_models(nf_ckpts).to(device)
+    models = ClassifierDatamodule.load_nf_models(nf_ckpts).to(device).eval().to(torch.float32)
     class_model_load = (
         CombinedClassifier.load_from_checkpoint(classifier_ckpt["classifier"]).to(device).eval().to(torch.float32)
     )

examples/fair_universe_demo/fair_universe_demo/utils/selection.py
@@ -596,11 +596,11 @@ def return1j2j(
         label_2j = label_2j[:nevents]
         label_1j = label_1j[:nevents]
 
-    data_1j = data_1j.to(device)
-    data_2j = data_2j.to(device)
+    data_1j = data_1j.to(device).to(torch.float32)
+    data_2j = data_2j.to(device).to(torch.float32)
     label_1j = label_1j.to(device)
     label_2j = label_2j.to(device)
-    models.to(device)
+    models.to(device).eval().to(torch.float32)
 
     with torch.no_grad():
         try:

examples/fair_universe_demo/fair_universe_demo/utils/stats.py
@@ -114,9 +114,9 @@ def compute_signal_fraction(
     -------
     A tuple (f_s_hat, theta_hat)
     """
-    test_data_2j = test_data_2j.to(eval_device)
-    test_data_1j = test_data_1j.to(eval_device)
-    dnn_model = dnn_model.to(eval_device)
+    test_data_2j = test_data_2j.to(eval_device).to(torch.float32)
+    test_data_1j = test_data_1j.to(eval_device).to(torch.float32)
+    dnn_model = dnn_model.to(eval_device).eval().to(torch.float32)
 
     nbins = 200
     bins = np.linspace(0, 1, num=nbins)